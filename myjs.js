30305ba6299eea59a0ea4f0f1c2460e141f4bc9a
jlord <jessica.lord@github.com>
2013-08-11 12:43:06 -0700

    mostly assigning templates

--- myjs.js
+++ myjs.js
@@ -9,8 +9,10 @@ var _ = require('underscore')
 glob("*.md", function(err, postFilenames) {
   if (err) return console.log(err)
   var postNames = postFilenames.map(function(name) {
+  	if (name === 'README') return
     return path.basename(name, '.md')
   })
+	console.log("Root Page Names", postNames)
   parseMarkdown(postNames, '/')
 })
 
@@ -19,35 +21,56 @@ glob("posts/*.md", function(err, postFilenames) {
   var postNames = postFilenames.map(function(name) {
     return path.basename(name, '.md')
   })
+	console.log("Post Page Names", postNames)
   parseMarkdown(postNames, '/posts/')
 })
-
-// function parseFileName() {
-
-// }
 
 function parseMarkdown(postFilenames, subDir) {
 	postFilenames.forEach(function(postName) {
 		var destinationDir = routeDestination(subDir)
 		var raw = fs.readFileSync(__dirname + subDir + postName + '.md').toString() 
   	var postContent = marked(raw)
-  	addToTemplate(postContent, postName, destinationDir)
-  	console.log('Parsed Markdown')
+  	assignTemplate(subDir, postContent, postName, destinationDir)
 	})
 }
 
-function addToTemplate(postContent, postName, destinationDir) {
-	var source = fs.readFileSync(__dirname + '/templates/single-post.html').toString()
+function assignTemplate(subDir, postContent, postName, destinationDir) {
+	var assignedTemplate = ""
+	var config = JSON.parse(fs.readFileSync('config.json'))
+	var templates = config.templates
+	if (subDir !== '/') {
+		for(var key in templates) {
+			console.log(key)
+			var values = templates[key] // an array
+			values.forEach(function(value) {
+				if (value.match(subDir)) assignedTemplate = key + '.html'
+			})
+		}
+	}
+	else {
+		for(var key in templates) {
+			var values = templates[key]
+			values.forEach(function(value) {
+				if (value.match(postName)) assignedTemplate = key + '.html'
+				else return
+			})
+		}
+	}
+	console.log("use this template", assignedTemplate)
+	// addToTemplate(postContent, postName, destinationDir, assignedTemplate)
+}
+
+function addToTemplate(postContent, postName, destinationDir, template) {
+	console.log("use this template", template)
+	var source = fs.readFileSync(__dirname + '/templates/' + template).toString()
   var template = handlebars.compile(source)
   var build = {content: postContent}
   writeFile(template(build), postName, destinationDir)
-  console.log('Added to Template')
 }
 
 function routeDestination(subDir) {
 	if (subDir === '/posts/') var destinationDir = '/blog/'
 	if (subDir === '/') var destinationDir = '/'
-	console.log('Routed Destination', destinationDir)
 	return destinationDir
 }
 
