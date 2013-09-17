ee6bf3ac088e2f4a7ffe4d969441219715a3ec55
Brian J Brennan <brianloveswords@gmail.com>
2013-09-17 10:15:03 +0200

    Allow different templates for different files

--- test/generate-site-dir.js
+++ test/generate-site-dir.js
@@ -7,28 +7,39 @@ function locate(file) {
   return path.join(__dirname, 'test-sitegen', file)
 }
 
+function readFile(file) {
+  return fs.readFileSync(locate(file)).toString('utf-8')
+}
+
 test('generating the site dir should work, ya?', function (t) {
   var expectFiles = [
+    'about.html',
+    'about.md',
     'blog',
     'blog/post.html',
     'blog/post.md',
     'page.html',
     'page.md',
-    'post1.html',
-    'post1.md'
-  ]
-  var expectContent = 'Site\n\n<h1>blog post</h1>\n<h2>author</h2>\n<h2>date</h2>\n<h2>tag, this, post</h2>\n<p>Hi</p>\n\n';
+  ].sort()
+
+  var expectPostContent = 'Site, blog\n\n<h1>blog post</h1>\n<h2>author</h2>\n<h2>date</h2>\n<h2>tag, this, post</h2>\n<p>Hi</p>\n\n'
+  var expectAboutContent = 'Site, about\n\n<p><em>about page</em></p>\n\n'
 
   genSiteDir({
     source: locate('content'),
     output: locate('site'),
     templateDir: locate('templates'),
+    templates: {
+      blog: 'blog/*',
+      about: 'about.md'
+    }
   }, function (err, result) {
-    var content = fs.readFileSync(locate('/site/blog/post.html')).toString('utf-8')
+    var aboutContent = readFile('/site/about.html')
+    var postContent = readFile('/site/blog/post.html')
 
-    console.dir(content)
-    t.same(result.files, expectFiles, 'should have all the right files!')
-    t.same(content, expectContent, 'should have the right content for posts')
+    t.same(result.files.sort(), expectFiles, 'should have all the right files!')
+    t.same(postContent, expectPostContent, 'should have the right content for posts')
+    t.same(aboutContent, expectAboutContent, 'should have the right content for about page')
     t.end()
   })
 })
